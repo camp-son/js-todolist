71f1a5dac52f381104df16737b294f57b3cc4b2a
camp-son <sjy2814@gmail.com>
2021-02-26 11:23:41 +0900

    localStorage로 데이터 저장

--- model/TodoModel.js
+++ model/TodoModel.js
@@ -2,6 +2,8 @@ import EventChannel from "../common/EventChannel.js";
 import { EventType } from "../constant/EventType.js";
 
 export class TodoModel extends EventChannel {
+    STORAGE_KEY = 'todos';
+
     constructor(url) {
         super();
         this.todos = [];
@@ -18,10 +20,22 @@ export class TodoModel extends EventChannel {
 
     getInitialDataHandler() {
         fetch(this.url).then(res => res.json()).then(todos => {
-            for (const todo of todos) {
+            const storeTodos = JSON.parse(localStorage.getItem(this.STORAGE_KEY));
+
+            storeTodos.reduce((summary, storeTodo) => {
+                const todo = todos.find((t) => t.id === storeTodo.id);
+
+                if (todo) {
+                    summary.push({...storeTodo, ...todo});
+                } else {
+                    summary.push(storeTodo);
+                }
+
+                return summary;
+            }, []).forEach((todo) => {
                 this.addTodo.call(this, todo);
-            }
-        });        
+            });
+        });
     }
 
     saveInitData(data) {
@@ -33,6 +47,8 @@ export class TodoModel extends EventChannel {
         if (!title) return ;
 
         this.todos = [...this.todos, {id: id || this.uniqueId, title: title, status: status || 'todo'}];
+
+        localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.todos));
         this.publish(EventType.CHANGE_TODO_LIST, this.todos);
     }
 
@@ -41,6 +57,8 @@ export class TodoModel extends EventChannel {
 
         if (findTodo) {
             findTodo.status = 'done';
+
+            localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.todos));
             this.publish(EventType.CHANGE_TODO_LIST, this.todos);
         }
     }
